20b6a38eb1125bf5f1bf1bd868216e226f77c111
Kevin <kevinkarelse1@gmail.com>
2021-05-12 00:18:03 +0200

    test

--- src/main/java/za/ac/cput/collectiontest/Vehicles.java
+++ src/main/java/za/ac/cput/collectiontest/Vehicles.java
@@ -1,13 +1,6 @@
 package za.ac.cput.collectiontest;
 
 public class Vehicles {
-    /**THIS TESTS WHAT BRANCH IM ON
-     * TESTING ARRAYS AND STUFF
-     *
-     *
-     */
-
-
-
+//just testing
 
 }
