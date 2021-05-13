9e5d4255d5fbe288cef7124ccbf95660840648e3
Kevin <kevinkarelse1@gmail.com>
2021-05-13 22:01:55 +0200

    Working on remove functionallity

--- src/test/java/za/ac/cput/collectiontest/VehiclesTest.java
+++ src/test/java/za/ac/cput/collectiontest/VehiclesTest.java
@@ -15,18 +15,19 @@ class VehiclesTest {
         Map<Integer, Vehicles> vehiclesMap = new HashMap<>();
         for (int i = 0; i < 10; i++){
             int id = i;
-            String brand = "BMW";
-            Vehicles vehicles = new Vehicles(i,brand);
+            String brand = "BMW";  //just a default name for testing purposes
+            Vehicles vehicles = new Vehicles(id,brand);
             vehiclesList.add(vehicles);
             vehiclesSet.add(vehicles);
             vehiclesMap.put(id, vehicles);
 
 
-
         }
+        //This tests if there is infact 10 objects that matches each particular collection interfaces size.
         assertEquals(10, vehiclesList.size());
         assertEquals(10, vehiclesSet.size());
         assertEquals(10, vehiclesMap.size());
+        //If test passes it means add function was properly implemented
     }
 
 
@@ -38,18 +39,22 @@ class VehiclesTest {
         Map<Integer, Vehicles> vehiclesMap = new HashMap<>();
         for (int i = 0; i < 10; i++){
             int id = i;
-            Vehicles vehicles = new Vehicles(id);
+            int index = 2;
+            String brand = "BMW" + i;
+            Vehicles vehicles = new Vehicles(id, brand);
             vehiclesList.add(vehicles);
             vehiclesSet.add(vehicles);
             vehiclesMap.put(id, vehicles);
 
-            vehiclesMap.remove(2);
+            vehiclesList.remove(1);
 
+            vehiclesMap.remove(index);
+            System.out.println(vehiclesList);
 
         }
 
-        //assertEquals(10, vehiclesList.size());
-        //assertEquals(10, vehiclesSet.size());
+        assertEquals(10, vehiclesList.size());
+        assertEquals(10, vehiclesSet.size());
         assertEquals(9, vehiclesMap.size());
     }
 
