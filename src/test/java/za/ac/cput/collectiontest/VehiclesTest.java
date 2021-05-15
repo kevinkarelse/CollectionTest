3fac6205bf7f81cd1b85ad8158d41ad16c620ff8
Kevin <kevinkarelse1@gmail.com>
2021-05-15 12:14:57 +0200

    Add and remove works properly

--- src/test/java/za/ac/cput/collectiontest/VehiclesTest.java
+++ src/test/java/za/ac/cput/collectiontest/VehiclesTest.java
@@ -40,9 +40,7 @@ class VehiclesTest {
             vehiclesMap.put(5,v5);
 
 
-
-
-        //This tests if there is in fact 10 objects that matches each particular collection interfaces size.
+        //This tests if there is in fact 5 objects that matches each particular collection interfaces size.
         assertEquals(5, vehiclesList.size());
         assertEquals(5, vehiclesSet.size());
         assertEquals(5, vehiclesMap.size());
@@ -53,28 +51,52 @@ class VehiclesTest {
     @Test
     //This tests using remove for collection
     void removeTest(){
+
         List<Vehicles> vehiclesList = new ArrayList<>();
         Set<Vehicles> vehiclesSet = new HashSet<>();
         Map<Integer, Vehicles> vehiclesMap = new HashMap<>();
-        for (int i = 0; i < 10; i++){
-            int id = i;
-            String brand = "BMW" + i;
-            Vehicles vehicles = new Vehicles(id, brand);
-            vehiclesList.add(vehicles);
-            vehiclesSet.add(vehicles);
-            vehiclesMap.put(id, vehicles);
 
-        }
-        //Now that the collections have been filled, we now remove one from the list
-        vehiclesList.remove(2);
+        Vehicles v1 = new Vehicles(1,"BMW");
+        Vehicles v2 = new Vehicles(2,"Toyota");
+        Vehicles v3 = new Vehicles(3,"Nissan");
+        Vehicles v4 = new Vehicles(4,"Kia");
+        Vehicles v5 = new Vehicles(5,"Porsche");
 
-        vehiclesMap.remove(2);
-        System.out.println(vehiclesSet);
+        //Populating each box with an object in List
+        vehiclesList.add(v1);
+        vehiclesList.add(v2);
+        vehiclesList.add(v3);
+        vehiclesList.add(v4);
+        vehiclesList.add(v5);
 
-        //As there were 10 boxes created originally, we check to see if one has in fact been removed by expecting only 9 boxes compared to size.
-        assertEquals(9, vehiclesList.size());
-        assertEquals(10, vehiclesSet.size());
-        assertEquals(9, vehiclesMap.size());
+        //Populating each box with an object in Set
+        vehiclesSet.add(v1);
+        vehiclesSet.add(v2);
+        vehiclesSet.add(v3);
+        vehiclesSet.add(v4);
+        vehiclesSet.add(v5);
+
+        //Populating each box with an object in Map
+        vehiclesMap.put(1,v1);
+        vehiclesMap.put(2,v2);
+        vehiclesMap.put(3,v3);
+        vehiclesMap.put(4,v4);
+        vehiclesMap.put(5,v5);
+
+        //Now that they are filled again for this test, we will be removing one object from each collection interface.
+        vehiclesList.remove(v2);
+        vehiclesSet.remove(v3);
+        vehiclesMap.remove(4);
+
+        System.out.println(vehiclesMap);
+
+
+
+        //As we have removed one object from each collection interface from the original 5, there should only be 4 filled.
+        assertEquals(4, vehiclesList.size());
+        assertEquals(4, vehiclesSet.size());
+        assertEquals(4, vehiclesMap.size());
+        //If test passes it means add function was properly implemented
     }
     @Test
         //This tests using remove for collection
