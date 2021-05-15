f2bef66e3ecbed11bb00c591695ae20958f61a94
Kevin <kevinkarelse1@gmail.com>
2021-05-15 12:01:54 +0200

    Add works on revised branch

--- src/test/java/za/ac/cput/collectiontest/VehiclesTest.java
+++ src/test/java/za/ac/cput/collectiontest/VehiclesTest.java
@@ -14,21 +14,38 @@ class VehiclesTest {
         List<Vehicles> vehiclesList = new ArrayList<>();
         Set<Vehicles> vehiclesSet = new HashSet<>();
         Map<Integer, Vehicles> vehiclesMap = new HashMap<>();
-        for (int i = 0; i < 10; i++){
-            int id = i;
-            String brand = "BMW" + i;  //just a default name for testing purposes
-            Vehicles vehicles = new Vehicles(id,brand);
-            vehiclesList.add(vehicles);
-            vehiclesSet.add(vehicles);
-            vehiclesMap.put(id, vehicles);
+
+            Vehicles v1 = new Vehicles(1,"BMW");
+            Vehicles v2 = new Vehicles(2,"Toyota");
+            Vehicles v3 = new Vehicles(3,"Nissan");
+            Vehicles v4 = new Vehicles(4,"Kia");
+            Vehicles v5 = new Vehicles(5,"Porsche");
+
+            vehiclesList.add(v1);
+            vehiclesList.add(v2);
+            vehiclesList.add(v3);
+            vehiclesList.add(v4);
+            vehiclesList.add(v5);
+
+            vehiclesSet.add(v1);
+            vehiclesSet.add(v2);
+            vehiclesSet.add(v3);
+            vehiclesSet.add(v4);
+            vehiclesSet.add(v5);
+
+            vehiclesMap.put(1,v1);
+            vehiclesMap.put(2,v2);
+            vehiclesMap.put(3,v3);
+            vehiclesMap.put(4,v4);
+            vehiclesMap.put(5,v5);
 
 
-        }
-        System.out.println(vehiclesList);
+
+
         //This tests if there is in fact 10 objects that matches each particular collection interfaces size.
-        assertEquals(10, vehiclesList.size());
-        assertEquals(10, vehiclesSet.size());
-        assertEquals(10, vehiclesMap.size());
+        assertEquals(5, vehiclesList.size());
+        assertEquals(5, vehiclesSet.size());
+        assertEquals(5, vehiclesMap.size());
         //If test passes it means add function was properly implemented
     }
 
